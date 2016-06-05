8b033bf0ec194ae721c064a25c0a6aecad8f96b9
LeeDongMin <admin@313.co.kr>
2016-06-05 17:39:01 +0900

    조원들 데이터 푸쉬.

---
+++ pages/twice/twice.js
@@ -0,0 +1,58 @@
+//var myModule = angular.module("todoApp",[]); // create module 
+
+angular.module('myApp', []).controller('todoCtrl',todoCtrl);
+
+var todoList = [
+	{done:true, title: "angular 독서"},
+	{done:false, title: "angular 스터디"},
+	{done:false, title: "개인 프로젝트 구성"}
+];
+
+function todoCtrl($scope){
+	$scope.appName = 'AngularJS To do App';
+	$scope.todoList = todoList;
+
+	// 추가
+	$scope.addNewTodo = function(newTitle){
+		todoList.push({done:false, title:newTitle});
+		$scope.newTitle = "";
+	}
+
+	// 보관
+	$scope.archive = function(){
+		for(var i=$scope.todoList.length-1; i>=0; i--){
+			//console.log(i);
+			if($scope.todoList[i].done){
+				$scope.todoList.splice(i,1);
+			}
+		}
+	}
+
+	// 남은 개수
+	$scope.remain = function(){
+		var remainCount = 0;
+		angular.forEach($scope.todoList, function(value, key){
+			if(value.done === false){
+				remainCount++;
+			}
+		});
+		return remainCount;
+	}
+
+	// 보관중인 목록
+	// $scope.doneTodo = function(){	
+	// 	for(var i=$scope.todoList.length-1; i>=0; i--){
+	// 		//console.log(i);
+	// 		if($scope.todoList[i].done == true){
+	// 			var add = $scope.todoList[i];
+	// 			var doneArray = $scope.doneList.splice(i,0,add);
+	// 			console.log(doneArray);
+	// 		}
+	// 		return doneArray;
+	// 	}
+	// }
+
+}
+
+
+

8db9dceb25b167fb2bf7cdf7fc0594953b6d8ab5
LeeDongMin <admin@313.co.kr>
2016-06-07 10:39:44 +0900

    1039

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict';
 
 var app=angular.module('projectWeb',[]);
-app.controller('indexCtrl', function($scope){
-  $scope.appName="AngularJS TODO APP";
-  $(document).ready(function () {
-		console.log('jquery Fire');
-	});
-});
+app.controller('indexCtrl', ['$scope', function($scope){
+	  $scope.appName="AngularJS TODO APP";
+	  $scope.$on('$includeContentLoaded', function(event) {
+		  var loader = document.createElement('script');
+		  loader.src = "dist/js/app.js";
+		  document.getElementsByTagName('head')[0].appendChild(loader);
+		  console.log('$includeContentLoaded');
+	  });
+	  $(document).ready(function () {
+		  console.log('jquery Fire');
+	  });
+	}]); 

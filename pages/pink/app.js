f004c7b9046ae3db5c47178af8e2fec6f97ca2c2
Ban Misun <jjaq88777@hanmail.net>
2016-06-13 17:03:02 +0900

    지시자 과제 진행중

--- pages/pink/app.js
+++ pages/pink/app.js
@@ -1,14 +1,6 @@
-// var todoList = [
-//  { done : true, title : "AngularJS 독서"},
-//  { done : false, title : "AngularJS 공부하기"},
-//  { done : false, title : "개인 프로젝트 구성"}
-// ];
-//
-// function todoCtrl ($scope) {
-//  $scope.appName = 'AngularJS TODO APP';
-//  }
+'use strict';
 
-var app=angular.module('myApp',[]);
+var app=angular.module('projectWeb',[]);
 app.controller('todoCtrl', function($scope){
   $scope.appName="AngularJS TODO APP";
   $scope.todoList=[
@@ -35,4 +27,37 @@ app.controller('todoCtrl', function($scope){
     });
     return remainCount;
   };
+});
+
+app.controller('menuCtrl',function($scope){
+  var item=[
+    {itemId: 1, menu:'샌드위치', cost:'2000', count:0},
+    {itemId:2, menu:'아메리카노', cost:'1000', count:0},
+    {itemId:3, menu:'카푸치노', cost:'1500', count:0},
+  ];
+  $scope.item=item;
+  $scope.totalPrice=0;//없으면 초기값이 없어서 총액에 아무것도 안찍힘
+  $scope.buy=function(){
+    $scope.totalPrice=0;//초기화를 안해주면 이전값을 가지고있어서 계산할 수록 누적됨
+    angular.forEach($scope.item, function(menu, idx){
+      $scope.totalPrice=$scope.totalPrice+ (menu.cost*menu.count);
+    });
+  };
+});
+
+app.controller('customer',function($scope){
+  var customers=[
+    {name:'김수현',age:'29'},
+    {name:'유아인',age:'31'},
+    {name:'주원',age:'30'}
+  ];
+  $scope.customers=customers;
+  var customObj={
+    name:'송중기',
+    age:'32',
+    job:'배우'
+  };
+  var star=['이동현','우규민','박용택'];
+  $scope.star=star;
+
 });

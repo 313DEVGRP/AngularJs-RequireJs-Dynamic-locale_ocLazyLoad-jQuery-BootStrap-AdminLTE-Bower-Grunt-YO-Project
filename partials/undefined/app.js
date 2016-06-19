ef9b806f87ae08ce6629dd1b7ad24ba91f6f1c2f
eyoho <wlgprk@gmail.com>
2016-06-20 02:05:08 +0900

    undefined 지시자 과제중..

--- partials/undefined/app.js
+++ partials/undefined/app.js
@@ -1,21 +1,26 @@
 var todoList = [
     {
         done: true,
-        title: "AngularJS 독서"
+        title: "AngularJS 독서",
+        status : "complete"
     },
     {
         done: false,
-        title: "AngularJS 공부하기"
+        title: "AngularJS 공부하기",
+        status : "ing"
     },
     {
         done: false,
-        title: "개인 프로젝트 구성"
+        title: "개인 프로젝트 구성",
+        status : "hold"
     }
 ];
 var app = angular.module('myApp', []).controller('todoCtrl', function($scope) {
 //function todoCtrl($scope) {
     $scope.appName = 'AngularJS TODO APP';
     $scope.todoList = todoList;
+    $scope.optStatus =["todo","progress","complete", "hold"]
+
     $scope.addNewTodo = function (newTitle) {
         todoList.push({
             done: false,
@@ -39,5 +44,9 @@ var app = angular.module('myApp', []).controller('todoCtrl', function($scope) {
         });
         return remainCount;
     };
-
+  $scope.optChange =  function (val, index) {
+       todoList[index].status = val;
+       todoList[index].done = (val == 'complete') ? true : false;
+       $scope.remain();
+  }
 });

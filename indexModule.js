615526a09f375bee07c906a8ac47ca72f715745b
LeeDongMin <admin@313.co.kr>
2016-06-21 18:55:45 +0900

    ui router 적용 완료 단. height 가 적용이 안되고 있음.
    ng include 의 문제로 파악되나. 동적 로딩과 충돌되어 FM 방식으로 처리 할 수 있는 
    방법을 연구해야 함. 
    
    이 고비만 넘기면 끝남.

--- indexModule.js
+++ indexModule.js
@@ -8,49 +8,43 @@ define(['projectWeb'], function () {
         function($stateProvider, $locationProvider, $urlRouterProvider, $ocLazyLoadProvider) {
 			
 			$urlRouterProvider.otherwise("/");
-			$locationProvider.hashPrefix('!');
+			$locationProvider.hashPrefix("StandardDevelopment#");
 			
-			$stateProvider
+			// You can also load via resolve
+		    $stateProvider
 		      .state('index', {
-		        url: "/index", // root route
+		        url: "/", // root route
 		        views: {
-		          "lazyLoadView": {
-		            controller: 'AppCtrl', // This view will use AppCtrl loaded below in the resolve
-		            templateUrl: 'partials/main.html'
+		          "indexView": {
+		            controller: 'partialIndexController', // This view will use AppCtrl loaded below in the resolve
+		            templateUrl: 'partials/index/partial-index.html'
 		          }
 		        },
 		        resolve: { // Any property in resolve should return a promise and is executed before the view is loaded
-		          loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
+		          indexCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
 		            // you can lazy load files for an existing module
-		            return $ocLazyLoad.load('js/AppCtrl.js');
+		            return $ocLazyLoad.load('partials/index/controller/index.js');
 		          }]
 		        }
 		      });
 	}]); //indexModule.config
 
 	indexModule.controller('indexController', ['$scope', '$ocLazyLoad', 
-        function($scope, $ocLazyLoad) {
-	
-			$scope.mainHeader = 'partials/index/header.html';
-			$scope.mainSidebar = 'partials/index/aside.html';
-			$scope.contentWrapper = 'partials/index/contents.html';
-			$scope.mainFooter = 'partials/index/footer.html';
-			$scope.controlSidebar = 'partials/index/sidebar.html';
-	
-			$ocLazyLoad.load([]);
-    
-			$scope.strutsiBatis = function() {
-				$ocLazyLoad.load('lazymodule').then(function() {
-				$scope.contentWrapper = 'partials/grid.html';
-				}, function(e) {
-					console.log(e);
-				});
-			}
-	}]);//indexModule.controller
-	
-	indexModule.bootstrap = function () {
-	    angular.bootstrap(document, ['indexModule']);
-	};
+           function($scope, $ocLazyLoad) {
+   	
+   			$scope.mainHeader = 'partials/index/header.html';
+   			$scope.mainSidebar = 'partials/index/aside.html';
+   			$scope.contentWrapper = 'partials/index/contents.html';
+   			$scope.mainFooter = 'partials/index/footer.html';
+   			$scope.controlSidebar = 'partials/index/sidebar.html';
+   	
+   			$ocLazyLoad.load([]);
+       
+   	}]);//indexModule.controller
+   	
+   	indexModule.bootstrap = function () {
+   	    angular.bootstrap(document, ['indexModule']);
+   	};
 
-	return indexModule;
+   	return indexModule;
 });

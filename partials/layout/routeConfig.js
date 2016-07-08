2ef32e8d2a5c2e8cd20c8a5db2f439e6dbdf8099
LeeDongMin <admin@313.co.kr>
2016-07-08 20:03:57 +0900

    dev site rival war site 분리 및 통합.

--- partials/layout/routeConfig.js
+++ partials/layout/routeConfig.js
@@ -32,6 +32,27 @@ define(['projectWeb'], function () {
 		            }]);
 		          }]
 		        }
+		      })
+		      .state('dev', {
+		        url: "/dev", // root route
+		        views: {
+		          '': {
+		            controller: 'layoutController', // This view will use AppCtrl loaded below in the resolve
+		            templateUrl: 'partials/layout/dev.html'
+		          }
+		        },
+		        resolve: { // Any property in resolve should return a promise and is executed before the view is loaded
+		          indexCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
+		            // you can lazy load files for an existing module
+		            return $ocLazyLoad.load([{
+		                name: 'layoutService',
+		                files: ['partials/layout/service.js']
+		            },{
+		                name: 'layoutController',
+		                files: ['partials/layout/controller.js']
+		            }]);
+		          }]
+		        }
 		      });
 	}]); //indexModule.config
 });

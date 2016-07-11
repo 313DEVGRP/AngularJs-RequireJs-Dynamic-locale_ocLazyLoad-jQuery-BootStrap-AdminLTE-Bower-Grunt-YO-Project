cb5defe4c3996a79a15639e34e6699cb5bc74ba5
kiuk <falhed7418@naver.com>
2016-07-11 21:59:53 +0900

    DataTable 추

--- partials/layout/contents/DEV/stakeholder/customers/service.js
+++ partials/layout/contents/DEV/stakeholder/customers/service.js
@@ -9,7 +9,41 @@ define(['projectWeb'], function () {
 		return {
 			fire: function() {
 				console.log('Customer');
-				//window.open('http://www.313.co.kr/confluence', 'confluence', '_blank');
+				
+				 function jstreeDataTableReload() {
+	        		  $('.bs-example-modal-sm').modal('show');
+	        		  $('#jstreeTable').dataTable().api().ajax.reload();
+	        	  }
+
+					$(function () {
+
+					var jstreeDataTable = $('#jstreeTable').dataTable( {
+						"ajax": {
+							"url": "/egovframework/com/etc/jstree/springiBatis/monitor/list.do",
+							"dataSrc": "rows"
+						},
+						"processing": true,
+						"responsive": true,
+						"paging": true,
+						"lengthChange": false,
+						"searching": false,
+						"ordering": true,
+						"info": true,
+						"autoWidth": false,
+						"columns": [
+							{ "data": "cell.0" },
+							{ "data": "cell.1" },
+							{ "data": "cell.2" },
+							{ "data": "cell.3" },
+							{ "data": "cell.4" },
+							{ "data": "cell.5" },
+							{ "data": "cell.6" },
+							{ "data": "cell.7" }
+						]
+					} );
+					});
+					// ==================== jstree DataTables ====================
+				
 			}//fire end
 		};//return end
 	}]);//.define function end

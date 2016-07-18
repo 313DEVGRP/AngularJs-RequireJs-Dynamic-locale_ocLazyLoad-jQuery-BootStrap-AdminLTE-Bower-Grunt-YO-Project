da801daa44101a5b5d03dd837e1fead2b7b4b532
Ban Misun <jjaq88777@hanmail.net>
2016-07-18 14:16:27 +0900

    숙제... 아마도 끝..?

--- partials/layout/contents/DEV/stakeholder/frontendDevelopers/controller.js
+++ partials/layout/contents/DEV/stakeholder/frontendDevelopers/controller.js
@@ -1,17 +1,21 @@
 'use strict';
 
 define(['projectWeb'], function () {
-	
+
 	var frontendDevelopersModule = angular.module('projectWeb', ['ui.router', 'oc.lazyLoad', 'frontendDevelopersService', 'devLayoutService']);
-	
+
 	frontendDevelopersModule.controller('frontendDevelopersController', ['$scope', '$ocLazyLoad', 'frontendDevelopersService', 'devLayoutService',
 	                                      	        function($scope, $ocLazyLoad, frontendDevelopersService, devLayoutService) {
-		
+
 		$ocLazyLoad.load([
 		                  'partials/common/js/jstree-v.pre1.0/_lib/jquery.cookie.js',
 		                  'partials/common/js/jstree-v.pre1.0/_lib/jquery.hotkeys.js',
 		                  'partials/common/js/jstree-v.pre1.0/jquery.jstree.js',
-		                  
+                      'partials/layout/contents/DEV/stakeholder/frontendDevelopers/jquery.mCustomScrollbar.min.js',
+                      'partials/layout/contents/DEV/stakeholder/frontendDevelopers/jquery.mousewheel.min.js',
+                      'partials/layout/contents/DEV/stakeholder/frontendDevelopers/jcarousel.min.js',
+                      'partials/layout/contents/DEV/stakeholder/frontendDevelopers/jquery.usquare.js',
+
 		                  'AdminLTE-2.3.3/plugins/datatables/jquery.dataTables.min.css',
 		                  'AdminLTE-2.3.3/plugins/datatables/dataTables.bootstrap.css',
 		              	  'AdminLTE-2.3.3/plugins/datatables/extensions/Responsive/css/dataTables.responsive.css'
@@ -19,9 +23,12 @@ define(['projectWeb'], function () {
           ]).then(function() {
         	  frontendDevelopersService.fire();
         	  devLayoutService.fire();
+            frontendDevelopersService.jcarousel();
+            frontendDevelopersService.uSquare();
+            frontendDevelopersService.customScroll();
 	    });
 
-		
+
 	}]);//frontendDevelopersController.controller
-	
+
 });

b20bfbbc7d915e4c9e624db91f17c2f1178a9dd3
LeeHyeJin <atelier.jiny@gmail.com>
2016-07-21 10:39:14 +0900

    systemEngineers update !

--- partials/layout/contents/DEV/stakeholder/systemEngineers/controller.js
+++ partials/layout/contents/DEV/stakeholder/systemEngineers/controller.js
@@ -1,27 +1,28 @@
 'use strict';
 
-define(['projectWeb'], function () {
-	
-	var confluenceModule = angular.module('projectWeb', ['ui.router', 'oc.lazyLoad', 'confluenceService', 'devLayoutService']);
-	
-	confluenceModule.controller('confluenceController', ['$scope', '$ocLazyLoad', 'confluenceService', 'devLayoutService',
-	                                      	        function($scope, $ocLazyLoad, confluenceService, devLayoutService) {
-		
-		$ocLazyLoad.load([
-		                  'partials/common/js/jstree-v.pre1.0/_lib/jquery.cookie.js',
-		                  'partials/common/js/jstree-v.pre1.0/_lib/jquery.hotkeys.js',
-		                  'partials/common/js/jstree-v.pre1.0/jquery.jstree.js',
-		                  
-		                  'AdminLTE-2.3.3/plugins/datatables/jquery.dataTables.min.css',
-		                  'AdminLTE-2.3.3/plugins/datatables/dataTables.bootstrap.css',
-		              	  'AdminLTE-2.3.3/plugins/datatables/extensions/Responsive/css/dataTables.responsive.css'
+define(['projectWeb'], function() {
 
-          ]).then(function() {
-        	  confluenceService.fire();
-        	  devLayoutService.fire();
-	    });
+    var systemEngineersModule = angular.module('projectWeb', ['ui.router', 'oc.lazyLoad', 'systemEngineersService', 'devLayoutService']);
 
-		
-	}]);//strutsiBatisController.controller
-	
+    systemEngineersModule.controller('systemEngineersController', ['$scope', '$ocLazyLoad', 'systemEngineersService', 'devLayoutService',
+        function($scope, $ocLazyLoad, systemEngineersService, devLayoutService) {
+
+            $ocLazyLoad.load([
+                'partials/common/js/jstree-v.pre1.0/_lib/jquery.cookie.js',
+                'partials/common/js/jstree-v.pre1.0/_lib/jquery.hotkeys.js',
+                'partials/common/js/jstree-v.pre1.0/jquery.jstree.js',
+
+                'AdminLTE-2.3.3/plugins/datatables/jquery.dataTables.min.css',
+                'AdminLTE-2.3.3/plugins/datatables/dataTables.bootstrap.css',
+                'AdminLTE-2.3.3/plugins/datatables/extensions/Responsive/css/dataTables.responsive.css'
+
+            ]).then(function() {
+                systemEngineersService.fire();
+                devLayoutService.fire();
+            });
+
+
+        }
+    ]); //systemEngineersController.controller
+
 });

d30e9d79af541279f475027bc50f46803a30c24e
Su-hyun <endband1978@gmail.com>
2016-07-28 12:25:41 +0900

    테스트 - 확인하고 삭제 할께요

--- partials/layout/contents/DEV/jstree/whyJsTree/controller.js
+++ partials/layout/contents/DEV/jstree/whyJsTree/controller.js
@@ -22,6 +22,8 @@ define(
 							$rootScope.$broadcast('goToHome');
 						};
 
+
+
 					}
 			]);// strutsiBatisHeaderController.controller
 
@@ -36,6 +38,13 @@ define(
 									function($scope, $ocLazyLoad,
 											whyJsTreeService, devLayoutService) {
 
+                    $scope.workList = [
+                      {num : "1", name: "html"},
+                      {num : "2", name: "css"},
+                      {num : "3", name: "js"},
+                      {num : "4", name: "Angular"}
+                    ];
+
 										$ocLazyLoad
 												.load(
 														['partials/common/js/jstree-v.pre1.0/_lib/jquery.cookie.js'
@@ -43,6 +52,14 @@ define(
 														]).then(function() {
 													whyJsTreeService.fire();
 													devLayoutService.fire();
+
+                          console.log("test");
+                          console.log($('.text-list'));
+                          console.log($('.text-list li'));
+                          console.log($('.text-list li p'));
+                          console.log($('.check-box'));
+                          console.log($('.check-01')); // 못찾음
+                          console.log($('.check-02')); // 못찾음
 												});
 
 									}

3a2fc008e1ae12eed210f4d05729c2fdb7d4a9df
Su-hyun <endband1978@gmail.com>
2017-01-16 16:39:12 +0900

    댓글 입력 진행 - 덧글의 덧글 + 클래스 부여 + show & hide 처리 남음

--- partials/layout/rivalWarLayoutController.js
+++ partials/layout/rivalWarLayoutController.js
@@ -896,7 +896,22 @@ define(
                 			brand: 'Apple'
 	                	}
 
-                	}
+                	};
+
+                /* 댓글 */
+
+                  $scope.directChatList = [
+                    {
+                      user : "Ryan Heywood",
+                      date : "01월 16일 14:59",
+                      text : "안드로이드의 경우 사용하지 않는 앱들이 설치되어 있는데 삭제도 안되니 더 불편한거 아닌가요?"
+                    },
+                    {
+                      user : "Ryan Heywood",
+                      date : "01월 16일 17:31",
+                      text : "아이폰 사용의 좋은 점이 분명히 있지만, 처음 사용자가 쓰기에는 어려움이 너무 많습니다."
+                    }
+                  ]
 
               }]);// rivalWarContentsController.controller
 

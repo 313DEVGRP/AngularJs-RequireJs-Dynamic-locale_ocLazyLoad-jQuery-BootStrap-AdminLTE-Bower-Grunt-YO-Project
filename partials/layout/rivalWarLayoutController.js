30da4035b49ab1497adff54287da6e8a561fac2b
Su-hyun <endband1978@gmail.com>
2017-01-17 16:03:31 +0900

    댓글 입력 진행 - 답글의 덧글 표시 남음

--- partials/layout/rivalWarLayoutController.js
+++ partials/layout/rivalWarLayoutController.js
@@ -900,20 +900,44 @@ define(
 
         /* 댓글 */
 
-        $scope.directChatList = [
+        $scope.directChatList = [ // 원글
           {
-            check : true,
-            user : "Ryan Heywood",
+            support : true,
+            userName : "Ryan Heywood",
+            userImg : "./partials/common/img/community/user1.jpg",
+            userRank : "./partials/common/img/community/level1.jpg",
             date : "01월 16일 14:59",
             text : "안드로이드의 경우 사용하지 않는 앱들이 설치되어 있는데 삭제도 안되니 더 불편한거 아닌가요?"
           },
           {
-            check : false,
-            user : "Ryan Heywood",
+            support : false,
+            userName : "Ryan Heywood",
+            userImg : "./partials/common/img/community/user2.jpg",
+            userRank : "./partials/common/img/community/level2.jpg",
             date : "01월 16일 17:31",
             text : "아이폰 사용의 좋은 점이 분명히 있지만, 처음 사용자가 쓰기에는 어려움이 너무 많습니다."
           }
-        ]
+        ];
+  
+        $scope.chatReplyList = [ // 답글
+          {
+            support : true,
+            userName : "Ryan Heywood",
+            userImg : "./partials/common/img/community/user1.jpg",
+            date : "01월 16일 14:59",
+            text : "안드로이드의 경우 사용하지 않는 앱들이 설치되어 있는데 삭제도 안되니 더 불편한거 아닌가요?"
+          },
+          {
+            support : false,
+            userName : "Ryan Heywood",
+            userImg : "./partials/common/img/community/user2.jpg",
+            date : "01월 16일 17:31",
+            text : "아이폰 사용의 좋은 점이 분명히 있지만, 처음 사용자가 쓰기에는 어려움이 너무 많습니다."
+          }
+        ];
+        
+        $scope.goReply = false;
+        $scope.replyTo = false;
 
       }]);// rivalWarContentsController.controller
 

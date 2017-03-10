31611c0690a07fe0b46c9b7dc49f94e09d8184d6
LeeDongMin <admin@313.co.kr>
2017-03-10 23:37:22 +0900

    [BT] SP-387 #resolve #time 1h #comment 앵귤러 Aside Controller에 완전히 분리하고 ajax 처리하여 메뉴를 붙이기 시작함 [RV] +review SR @admin

--- partials/layout/aside/rivalWar/asideController.js
+++ partials/layout/aside/rivalWar/asideController.js
@@ -2,24 +2,41 @@
 
 define(['projectWeb'], function () {
 
-  var rivalWarAsideControllerModule = angular.module('projectWeb', ['ui.router', 'oc.lazyLoad']);
+  var rivalWarAsideControllerModule = angular.module('projectWeb', ['ui.router', 'oc.lazyLoad', 'rivalWarLayoutService']);
 
-  rivalWarAsideControllerModule.controller('rivalWarAsideController', ['$scope', '$ocLazyLoad',
-    function ($scope, $ocLazyLoad) {
+  rivalWarAsideControllerModule.controller('rivalWarAsideController', ['$scope', '$ocLazyLoad', 'rivalWarLayoutService', '$http',
+    function ($scope, $ocLazyLoad, rivalWarLayoutService, $http) {
 
       console.log('rivalWarAsideController');
 
-      var $repeatList = $('.repeatList > li'), idx = 0;
+      /* 파라메터로 보낼 임의의 데이터 객체 */
+      var dataObject =
+      {
+        dataNo : $scope.dataNo + "",
+        dataName : $scope.dataName,
+        dataContent : $scope.dataContent
+      };
 
-      $repeatList.eq(idx).addClass('on');
-
-      var asideRelList = setInterval(function () {
-        $repeatList.eq(idx).addClass('on').siblings().removeClass();
-        idx += 1;
-        if (idx === $repeatList.length) idx = 0;
-        $repeatList.eq(idx).addClass('on').siblings().removeClass();
-      }, 4000);
-
+      /* AJAX 통신 처리 */
+      $http({
+        method: 'GET', //방식
+        url: 'http://www.313.co.kr/rivalWar/api/menu/getChildMenu.do?c_id=2', /* 통신할 URL */
+        /* data: dataObject, 파라메터로 보낼 데이터 */
+        headers: {'Content-Type': 'application/json; charset=utf-8'} //헤더
+      })
+        .success(function(data, status, headers, config) {
+          if( data ) {
+            /* 성공적으로 결과 데이터가 넘어 왔을 때 처리 */
+            $scope.warList = data;
+          }
+          else {
+            /* 통신한 URL에서 데이터가 넘어오지 않았을 때 처리 */
+          }
+        })
+        .error(function(data, status, headers, config) {
+          /* 서버와의 연결이 정상적이지 않을 때 처리 */
+          console.log(status);
+        });
 
     }]);
 

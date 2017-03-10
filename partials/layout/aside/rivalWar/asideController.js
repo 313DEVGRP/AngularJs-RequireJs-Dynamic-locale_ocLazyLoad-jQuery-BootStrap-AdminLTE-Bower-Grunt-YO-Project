af73b39048f1b89ec40714ed3bacc7807a248c0e
LeeDongMin <admin@313.co.kr>
2017-03-10 09:34:11 +0900

    [BT] SP-384 #resolve #time 1h #comment 앵귤러 Aside Controller 구조적으로 내포된 잠재된 문제를 해결함. [RV] +review SR @admin

--- partials/layout/aside/rivalWar/asideController.js
+++ partials/layout/aside/rivalWar/asideController.js
@@ -9,21 +9,16 @@ define(['projectWeb'], function () {
 
       console.log('rivalWarAsideController');
 
-      $ocLazyLoad.load(['partials/common/js/jstree-v.pre1.0/_lib/jquery.cookie.js'])
-        .then(function () {
-          var $repeatList = $('.repeatList > li'),
-            idx = 0;
+      var $repeatList = $('.repeatList > li'), idx = 0;
 
-          $repeatList.eq(idx).addClass('on');
+      $repeatList.eq(idx).addClass('on');
 
-          var asideRelList = setInterval(function () {
-            rivalWarLayoutService.addOn($repeatList.eq(idx));
-            idx += 1;
-            if (idx === $repeatList.length) idx = 0;
-            rivalWarLayoutService.addOn($repeatList.eq(idx));
-          }, 4000);
-
-        });
+      var asideRelList = setInterval(function () {
+        rivalWarLayoutService.addOn($repeatList.eq(idx));
+        idx += 1;
+        if (idx === $repeatList.length) idx = 0;
+        rivalWarLayoutService.addOn($repeatList.eq(idx));
+      }, 4000);
 
 
     }]);

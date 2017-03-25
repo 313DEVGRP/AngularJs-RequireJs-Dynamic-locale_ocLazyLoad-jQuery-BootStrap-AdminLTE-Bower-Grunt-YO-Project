437e3b932632942baac5d6c25953deb474365fac
hyejinlee <atelier.jiny@gmail.com>
2017-03-25 16:47:04 +0900

    level img update

--- partials/layout/contents/rivalWar/contentsChatController.js
+++ partials/layout/contents/rivalWar/contentsChatController.js
@@ -16,7 +16,7 @@ define(['projectWeb'], function () {
           support: true,
           userName: "Ryan Heywood",
           userImg: "./partials/common/img/community/user1.jpg",
-          userRank: "./partials/common/img/community/level1.jpg",
+          userRank: "./partials/common/img/community/level1.png",
           date: "01월 16일 14:59",
           text: "안드로이드의 경우 사용하지 않는 앱들이 설치되어 있는데 삭제도 안되니 더 불편한거 아닌가요?"
         },
@@ -24,7 +24,7 @@ define(['projectWeb'], function () {
           support: false,
           userName: "Ryan Heywood",
           userImg: "./partials/common/img/community/user2.jpg",
-          userRank: "./partials/common/img/community/level2.jpg",
+          userRank: "./partials/common/img/community/level2.png",
           date: "01월 16일 17:31",
           text: "아이폰 사용의 좋은 점이 분명히 있지만, 처음 사용자가 쓰기에는 어려움이 너무 많습니다."
         }

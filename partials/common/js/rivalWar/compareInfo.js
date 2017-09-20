e61d68fe7a5e990aeb4e49db468574b4f7f03a98
hyejinlee <atelier.jiny@gmail.com>
2017-09-20 21:52:53 +0900

    compareInfo js update :)

--- partials/common/js/rivalWar/compareInfo.js
+++ partials/common/js/rivalWar/compareInfo.js
@@ -10,7 +10,7 @@ $(function () {
     console.log(responseData);
     $("#compareInfoTime").text(responseData.calTime);
 
-    $("#botImage").attr("href", responseData.botImageUrl1);
+    $("#botImage").attr("src", responseData.botImageUrl1);
 
     $("#botVender").attr("href", responseData.botVenderUrl);
     $("#botVender").html(responseData.botVenderText + " <i class='fa fa-arrow-circle-right'></i>");
@@ -33,43 +33,43 @@ $(function () {
     //mid
     $("#midImage").attr("src", responseData.midImageUrl1);
 
-    $("#midVender").attr("src", responseData.midVenderUrl);
+    $("#midVender").attr("href", responseData.midVenderUrl);
     $("#midVender").html(responseData.midVenderText + " <i class='fa fa-arrow-circle-right'></i>");
 
-    $("#midPost1").attr("src", responseData.midPost1Url);
+    $("#midPost1").attr("href", responseData.midPost1Url);
     $("#midPost1").html("<img src='" + responseData.midPost1Icon + "'> "+ responseData.midPost1Text);
 
-    $("#midPost2").attr("src", responseData.midPost2Url);
+    $("#midPost2").attr("href", responseData.midPost2Url);
     $("#midPost2").html("<img src='" + responseData.midPost2Icon + "'> "+ responseData.midPost2Text);
 
-    $("#midPost3").attr("src", responseData.midPost3Url);
+    $("#midPost3").attr("href", responseData.midPost3Url);
     $("#midPost3").html("<img src='" + responseData.midPost3Icon + "'> "+ responseData.midPost3Text);
 
-    $("#midPost4").attr("src", responseData.midPost4Url);
+    $("#midPost4").attr("href", responseData.midPost4Url);
     $("#midPost4").html("<img src='" + responseData.midPost4Icon + "'> "+ responseData.midPost4Text);
 
-    $("#midPost5").attr("src", responseData.midPost5Url);
+    $("#midPost5").attr("href", responseData.midPost5Url);
     $("#midPost5").html("<img src='" + responseData.midPost5Icon + "'> "+ responseData.midPost5Text);
 
     //top
     $("#topImage").attr("src", responseData.topImageUrl1);
 
-    $("#topVender").attr("src", responseData.topVenderUrl);
+    $("#topVender").attr("href", responseData.topVenderUrl);
     $("#topVender").html(responseData.topVenderText + " <i class='fa fa-arrow-circle-right'></i>");
 
-    $("#topPost1").attr("src", responseData.topPost1Url);
+    $("#topPost1").attr("href", responseData.topPost1Url);
     $("#topPost1").html("<img src='" + responseData.topPost1Icon + "'> "+ responseData.topPost1Text);
 
-    $("#topPost2").attr("src", responseData.topPost2Url);
+    $("#topPost2").attr("href", responseData.topPost2Url);
     $("#topPost2").html("<img src='" + responseData.topPost2Icon + "'> "+ responseData.topPost2Text);
 
-    $("#topPost3").attr("src", responseData.topPost3Url);
+    $("#topPost3").attr("href", responseData.topPost3Url);
     $("#topPost3").html("<img src='" + responseData.topPost3Icon + "'> "+ responseData.topPost3Text);
 
-    $("#topPost4").attr("src", responseData.topPost4Url);
+    $("#topPost4").attr("href", responseData.topPost4Url);
     $("#topPost4").html("<img src='" + responseData.topPost4Icon + "'> "+ responseData.topPost4Text);
 
-    $("#topPost5").attr("src", responseData.topPost5Url);
+    $("#topPost5").attr("href", responseData.topPost5Url);
     $("#topPost5").html("<img src='" + responseData.topPost5Icon + "'> "+ responseData.topPost5Text);
   }
   function compareInfoErrorCallback(){ console.log("errorCallback"); }

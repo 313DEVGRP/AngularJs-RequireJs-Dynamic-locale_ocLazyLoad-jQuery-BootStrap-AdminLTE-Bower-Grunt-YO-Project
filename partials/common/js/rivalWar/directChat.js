05f15653cb668d6a4956e721a38f83dd5dae5a36
LeeDongMin <admin@313.co.kr>
2017-11-17 11:29:51 +0900

    [BT] SP-488 #close #time 1h #comment 댓글 코드 삽입 ( 리팩토링 필요 확인 필요 ) [RV] +review SR @admin

--- partials/common/js/rivalWar/directChat.js
+++ partials/common/js/rivalWar/directChat.js
@@ -1,5 +1,5 @@
 $(function () {
-  console.log("menu script done");
+  console.log("directChat script done");
 
   // 메뉴 API
   var ajaxParamType = 'noneParam';
@@ -32,6 +32,7 @@ function directChatAjaxCall(jsonData){
   $('#chat-messages').empty(); //패널(div)의 내용 초기화
   var table = "";
   $(jsonData).each(function (chatIndex, chatList){
+
     table += "<div class='chat-message'>";
 
     table += "  <div class='sender pull-left'>";
@@ -67,11 +68,68 @@ function directChatAjaxCall(jsonData){
     table += "        <i class='fa fa-bell-o'></i>" + "신고";
     table += "      </button>";
     table += "    </div>";
-    table += "  </div>";
 
+    replyChatAjaxCall(chatList, table);
+
+    table += "  </div>";
     table += "</div>";
+
   });
   $('#chat-messages').append(table); //패널에 추가하기
 }
 
+function replyChatAjaxCall(chatList, table){
 
+  console.log("menu script done");
+
+  // 메뉴 API
+  var ajaxParamType = 'noneParam';
+  var ajaxParam = '';
+  var ajaxURL = '/api/rivalWar/directChat/getPaginatedChildNode.do?c_id=' + chatList.c_id + '&pageIndex=1&pageUnit=1&pageSize=100';
+  var ajaxType = 'GET';
+  var ajaxReturnType = 'json';
+
+  function beforeSendCallback() {
+    console.log("beforeSendCallback");
+  }
+
+  function successCallback(responseData) {
+
+    table += "<ul class='reply-wrap'>";
+
+    $(responseData.result).each(function (replyIndex, replyList){
+
+      table += "  <li class='reply-msg clearfix'>";
+      table += "    <div class='reply-content'>";
+      table += "      <div class='written-info'>";
+      table += "        <span class='user-img'>";
+      table += "          <img alt='message user image' src='partials/common/img/custom/user1.png'>";
+      table += "        </span>";
+      table += "        <span class='direct-chat-name pull-left'>" + replyList.userId + "</span>";
+      table += "        <span class='direct-chat-timestamp pull-right'>" + replyList.writeTime + "</span>";
+      table += "      </div>";
+      table += "      <p>" + replyList.contentsBody + "</p>";
+      table += "      <div>";
+      table += "        <button class='info-box-icon chat-type type-up'>" + "좋아요" + replyList.likeCount + "</button>";
+      table += "        <button class='info-box-icon chat-type type-reply'>" + "답글 달기" + "</button>";
+      table += "      </div>";
+      table += "   </div>";
+      table += "  </li>";
+
+    });
+
+    table += "</ul>";
+
+  }
+
+  function errorCallback() {
+    console.log("errorCallback");
+  }
+
+  function completeCallback() {
+    console.log("completeCallback");
+  }
+
+  callAjax(ajaxParamType, ajaxParam, ajaxURL, ajaxType, ajaxReturnType, beforeSendCallback, successCallback, errorCallback, completeCallback);
+
+}
